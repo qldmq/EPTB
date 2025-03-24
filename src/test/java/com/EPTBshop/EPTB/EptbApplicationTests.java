3392587663f2f7557b3daacf353c90e4cc38d651
qldmq <ksh13345@naver.com>
2025-03-24 17:15:41 +0900

    기본 설정

---
+++ src/test/java/com/EPTBshop/EPTB/EptbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EPTBshop.EPTB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EptbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
