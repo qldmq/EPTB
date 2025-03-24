9382096b1c95894d952762bab51821deae1a50e8
qldmq <ksh13345@naver.com>
2025-03-25 01:50:45 +0900

    백엔드 기본 설정

---
+++ eptb-backend/src/main/java/com/EPTBshop/EPTB/EptbApplication.java
@@ -0,0 +1,13 @@
+package com.EPTBshop.EPTB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EptbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EptbApplication.class, args);
+	}
+
+}
